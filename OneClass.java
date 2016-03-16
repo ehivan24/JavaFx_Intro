6e8ff13ff1b71da35cbb8151cbbee05f5cb98df2
Hivan <ehivan24@hotmail.com>
2016-03-16 11:49:59 -0400

    Introduction to JavaFx

---
+++ OneClass.java
@@ -0,0 +1,86 @@
+
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+
+public class OneClass extends Application {
+
+	public static void main(String[] args){
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(25,25,25,25));
+				
+		Text sceneTitle = new Text("Welcome");
+		sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+		grid.add(sceneTitle, 0, 0,2,1);
+		
+		Label userName = new Label("User Name:");
+		grid.add(userName, 0, 1);
+		
+		TextField textField = new TextField();
+		grid.add(textField, 1, 1);
+		
+		Label pw = new Label("Password:");
+		grid.add(pw, 0, 2);
+
+		PasswordField pwBox = new PasswordField();
+		grid.add(pwBox, 1, 2);
+
+		Button btn = new Button();
+		HBox hbBtn = new HBox(10);
+		btn.setText("Save");
+
+		grid.add(btn, 1, 4);
+		
+		final Text actionTarget = new Text();
+		grid.add(actionTarget, 1, 6);
+		
+		Scene scene = new Scene(grid, 300, 250);
+		primaryStage.setTitle("Hello Worlds");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+		btn.setOnAction(new EventHandler<ActionEvent>(){
+
+			@Override
+			public void handle(ActionEvent event) {
+				
+				String name = textField.getText().toString();
+				String pass = pwBox.getText().toString();
+				if((name.length() == 0) || (pass.length() == 0)){
+					actionTarget.setText("Enter Credentials");
+					actionTarget.setFill(Color.FIREBRICK);
+				}else{
+					actionTarget.setText("");
+					System.out.println("Username: "+ name + " Password: "+ pass);
+				}
+			}			
+		});
+		
+		
+	}
+}
